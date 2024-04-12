28aa509024d9f5ed503234e472ac97603439a3e8
Frederic Bessette <msfredb7@gmail.com>
2024-04-12 14:05:33 -0400

    improve FixRandom distribution by using builtin Unity.Mathematics.Random

--- Runtime/FixRandom.cs
+++ Runtime/FixRandom.cs
@@ -1,388 +1,95 @@
 using System;
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
+using UnityEngine;
 
-/// <summary>
-/// A fast random number generator for .NET
-/// Colin Green, January 2005
-/// 
-/// September 4th 2005
-///	 Added NextBytesUnsafe() - commented out by default.
-///	 Fixed bug in Reinitialise() - y,z and w variables were not being reset.
-/// 
-/// Key points:
-///  1) Based on a simple and fast xor-shift pseudo random number generator (RNG) specified in: 
-///  Marsaglia, George. (2003). Xorshift RNGs.
-///  http://www.jstatsoft.org/v08/i14/xorshift.pdf
-///  
-///  This particular implementation of xorshift has a period of 2^128-1. See the above paper to see
-///  how this can be easily extened if you need a longer period. At the time of writing I could find no 
-///  information on the period of System.Random for comparison.
-/// 
-///  2) Faster than System.Random. Up to 8x faster, depending on which methods are called.
-/// 
-///  3) Direct replacement for System.Random. This class implements all of the methods that System.Random 
-///  does plus some additional methods. The like named methods are functionally equivalent.
-///  
-///  4) Allows fast re-initialisation with a seed, unlike System.Random which accepts a seed at construction
-///  time which then executes a relatively expensive initialisation routine. This provides a vast speed improvement
-///  if you need to reset the pseudo-random number sequence many times, e.g. if you want to re-generate the same
-///  sequence many times. An alternative might be to cache random numbers in an array, but that approach is limited
-///  by memory capacity and the fact that you may also want a large number of different sequences cached. Each sequence
-///  can each be represented by a single seed value (int) when using FastRandom.
-///  
-///  Notes.
-///  A further performance improvement can be obtained by declaring local variables as static, thus avoiding 
-///  re-allocation of variables on each call. However care should be taken if multiple instances of
-///  FastRandom are in use or if being used in a multi-threaded environment.
-/// 
-/// </summary>
 public struct FixRandom
 {
-    // fbessette: not deterministic because of double calculation
+    Unity.Mathematics.Random _random;
 
-    // The +1 ensures NextDouble doesn't generate 1.0
-    //const double REAL_UNIT_INT = 1.0 / ((double)int.MaxValue + 1.0);
-    //const double REAL_UNIT_UINT = 1.0 / ((double)uint.MaxValue + 1.0);
+    /// <summary>Initialises a new instance using an int value as seed.</summary>
+    public FixRandom(int seed) : this((uint)seed) { }
 
-    const uint Y = 842502087, Z = 3579807591, W = 273326509;
-
-    uint x, y, z, w;
-
-    // Buffer 32 bits in bitBuffer, return 1 at a time, keep track of how many have been returned
-    // with bitBufferIdx.
-    uint bitBuffer;
-    uint bitMask;
-
-    #region Constructors
-
-    /// <summary>
-    /// Initialises a new instance using an int value as seed.
-    /// This constructor signature is provided to maintain compatibility with
-    /// System.Random
-    /// </summary>
-    public FixRandom(int seed)
+    /// <summary>Initialises a new instance using an int value as seed.</summary>
+    public FixRandom(uint seed)
     {
-        x = 0;
-        y = 0;
-        z = 0;
-        w = 0;
-        bitBuffer = 0;
-        bitMask = 1;
-
-        Reinitialise(seed);
+        _random = new(seed);
     }
 
-    #endregion
-
-    #region Public Methods [Reinitialisation]
-
-    /// <summary>
-    /// Reinitialises using an int value as a seed.
-    /// </summary>
-    /// <param name="seed"></param>
-    public void Reinitialise(int seed)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static FixRandom CreateFromIndex(uint index)
     {
-        // The only stipulation stated for the xorshift RNG is that at least one of
-        // the seeds x,y,z,w is non-zero. We fulfill that requirement by only allowing
-        // resetting of the x seed
-        x = (uint)seed;
-        y = Y;
-        z = Z;
-        w = W;
-
-        int mod = seed % 7;
-        for (int i = 0; i < mod; i++)
-        {
-            Next();
-        }
+        var result = new FixRandom();
+        result._random = Unity.Mathematics.Random.CreateFromIndex(index);
+        return result;
     }
 
-    #endregion
+    /// <summary>Returns a uniformly random uint value in the interval [0, 4294967294].</summary>
+    /// <returns>A uniformly random unsigned integer.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public uint NextUInt() => _random.NextUInt();
 
-    #region Public Methods [System.Random functionally equivalent methods]
+    /// <summary>Returns a uniformly random int value in the interval [-2147483647, 2147483647].</summary>
+    /// <returns>A uniformly random integer value.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int NextInt() => _random.NextInt();
 
-    /// <summary>
-    /// Generates a random int over the range 0 to int.MaxValue-1.
-    /// MaxValue is not generated in order to remain functionally equivalent to System.Random.Next().
-    /// This does slightly eat into some of the performance gain over System.Random, but not much.
-    /// For better performance see:
-    /// 
-    /// Call NextInt() for an int over the range 0 to int.MaxValue.
-    /// 
-    /// Call NextUInt() and cast the result to an int to generate an int over the full Int32 value range
-    /// including negative values. 
-    /// </summary>
-    public int Next()
-    {
-        uint t = (x ^ (x << 11));
-        x = y; y = z; z = w;
-        w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
-
-        // Handle the special case where the value int.MaxValue is generated. This is outside of 
-        // the range of permitted values, so we therefore call Next() to try again.
-        uint rtn = w & 0x7FFFFFFF;
-        if (rtn == 0x7FFFFFFF)
-            return Next();
-        return (int)rtn;
-    }
+    /// <summary>Returns a uniformly random bool value.</summary>
+    /// <returns>A uniformly random boolean value.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool NextBool() => _random.NextBool();
 
 
-    // fbessette: not deterministic because of double calculation
+    /// <summary>Returns a uniformly random fix value in the interval [0, 1].</summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public fix NextFixRatio() => ((new fix(NextInt(0, int.MaxValue)) / new fix(int.MaxValue)) + 1) / (fix)2;
 
-    ///// <summary>
-    ///// Generates a random int over the range 0 to upperBound-1, and not including upperBound.
-    ///// </summary>
-    ///// <param name="upperBound"></param>
-    //public int Next(int upperBound)
-    //{
-    //    if (upperBound < 0)
-    //        throw new ArgumentOutOfRangeException("upperBound", upperBound, "upperBound must be >=0");
-
-    //    uint t = (x ^ (x << 11));
-    //    x = y; y = z; z = w;
-
-    //    // The explicit int cast before the first multiplication gives better performance.
-    //    // See comments in NextDouble.
-    //    return (int)((REAL_UNIT_INT * (int)(0x7FFFFFFF & (w = (w ^ (w >> 19)) ^ (t ^ (t >> 8))))) * upperBound);
-    //}
-
-    // fbessette: not deterministic because of double calculation
-
-    ///// <summary>
-    ///// Generates a random int over the range lowerBound to upperBound-1, and not including upperBound.
-    ///// upperBound must be >= lowerBound. lowerBound may be negative.
-    ///// </summary>
-    ///// <param name="lowerBound"></param>
-    ///// <param name="upperBound"></param>
-    ///// <returns></returns>
-    //public int Next(int lowerBound, int upperBound)
-    //{
-    //    if (lowerBound > upperBound)
-    //        throw new ArgumentOutOfRangeException("upperBound", upperBound, "upperBound must be >=lowerBound");
-
-    //    uint t = (x ^ (x << 11));
-    //    x = y; y = z; z = w;
-
-    //    // The explicit int cast before the first multiplication gives better performance.
-    //    // See comments in NextDouble.
-    //    int range = upperBound - lowerBound;
-    //    if (range < 0)
-    //    {   // If range is <0 then an overflow has occured and must resort to using long integer arithmetic instead (slower).
-    //        // We also must use all 32 bits of precision, instead of the normal 31, which again is slower.	
-    //        return lowerBound + (int)((REAL_UNIT_UINT * (double)(w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)))) * (double)((long)upperBound - (long)lowerBound));
-    //    }
-
-    //    // 31 bits of precision will suffice if range<=int.MaxValue. This allows us to cast to an int and gain
-    //    // a little more performance.
-    //    return lowerBound + (int)((REAL_UNIT_INT * (double)(int)(0x7FFFFFFF & (w = (w ^ (w >> 19)) ^ (t ^ (t >> 8))))) * (double)range);
-    //}
-
-    // fbessette: not deterministic because of double calculation
-
-    ///// <summary>
-    ///// Generates a random double. Values returned are from 0.0 up to but not including 1.0.
-    ///// </summary>
-    ///// <returns></returns>
-    //public double NextDouble()
-    //{
-    //    uint t = (x ^ (x << 11));
-    //    x = y; y = z; z = w;
-
-    //    // Here we can gain a 2x speed improvement by generating a value that can be cast to 
-    //    // an int instead of the more easily available uint. If we then explicitly cast to an 
-    //    // int the compiler will then cast the int to a double to perform the multiplication, 
-    //    // this final cast is a lot faster than casting from a uint to a double. The extra cast
-    //    // to an int is very fast (the allocated bits remain the same) and so the overall effect 
-    //    // of the extra cast is a significant performance improvement.
-    //    //
-    //    // Also note that the loss of one bit of precision is equivalent to what occurs within 
-    //    // System.Random.
-    //    return REAL_UNIT_INT * (int)(0x7FFFFFFF & (w = (w ^ (w >> 19)) ^ (t ^ (t >> 8))));
-    //}
-
-
-    /// <summary>
-    /// Fills the provided byte array with random bytes.
-    /// This method is functionally equivalent to System.Random.NextBytes(). 
-    /// </summary>
-    /// <param name="buffer"></param>
-    public void NextBytes(byte[] buffer)
-    {
-        // Fill up the bulk of the buffer in chunks of 4 bytes at a time.
-        uint x = this.x, y = this.y, z = this.z, w = this.w;
-        int i = 0;
-        uint t;
-        for (int bound = buffer.Length - 3; i < bound;)
-        {
-            // Generate 4 bytes. 
-            // Increased performance is achieved by generating 4 random bytes per loop.
-            // Also note that no mask needs to be applied to zero out the higher order bytes before
-            // casting because the cast ignores thos bytes. Thanks to Stefan Troschütz for pointing this out.
-            t = (x ^ (x << 11));
-            x = y; y = z; z = w;
-            w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
-
-            buffer[i++] = (byte)w;
-            buffer[i++] = (byte)(w >> 8);
-            buffer[i++] = (byte)(w >> 16);
-            buffer[i++] = (byte)(w >> 24);
-        }
-
-        // Fill up any remaining bytes in the buffer.
-        if (i < buffer.Length)
-        {
-            // Generate 4 bytes.
-            t = (x ^ (x << 11));
-            x = y; y = z; z = w;
-            w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
-
-            buffer[i++] = (byte)w;
-            if (i < buffer.Length)
-            {
-                buffer[i++] = (byte)(w >> 8);
-                if (i < buffer.Length)
-                {
-                    buffer[i++] = (byte)(w >> 16);
-                    if (i < buffer.Length)
-                    {
-                        buffer[i] = (byte)(w >> 24);
-                    }
-                }
-            }
-        }
-        this.x = x; this.y = y; this.z = z; this.w = w;
-    }
-
-
-    //		/// <summary>
-    //		/// A version of NextBytes that uses a pointer to set 4 bytes of the byte buffer in one operation
-    //		/// thus providing a nice speedup. The loop is also partially unrolled to allow out-of-order-execution,
-    //		/// this results in about a x2 speedup on an AMD Athlon. Thus performance may vary wildly on different CPUs
-    //		/// depending on the number of execution units available.
-    //		/// 
-    //		/// Another significant speedup is obtained by setting the 4 bytes by indexing pDWord (e.g. pDWord[i++]=w)
-    //		/// instead of adjusting it dereferencing it (e.g. *pDWord++=w).
-    //		/// 
-    //		/// Note that this routine requires the unsafe compilation flag to be specified and so is commented out by default.
-    //		/// </summary>
-    //		/// <param name="buffer"></param>
-    //		public unsafe void NextBytesUnsafe(byte[] buffer)
-    //		{
-    //			if(buffer.Length % 8 != 0)
-    //				throw new ArgumentException("Buffer length must be divisible by 8", "buffer");
-    //
-    //			uint x=this.x, y=this.y, z=this.z, w=this.w;
-    //			
-    //			fixed(byte* pByte0 = buffer)
-    //			{
-    //				uint* pDWord = (uint*)pByte0;
-    //				for(int i=0, len=buffer.Length>>2; i < len; i+=2) 
-    //				{
-    //					uint t=(x^(x<<11));
-    //					x=y; y=z; z=w;
-    //					pDWord[i] = w = (w^(w>>19))^(t^(t>>8));
-    //
-    //					t=(x^(x<<11));
-    //					x=y; y=z; z=w;
-    //					pDWord[i+1] = w = (w^(w>>19))^(t^(t>>8));
-    //				}
-    //			}
-    //
-    //			this.x=x; this.y=y; this.z=z; this.w=w;
-    //		}
-
-    #endregion
-
-    #region Public Methods [Methods not present on System.Random]
-
-    /// <summary>
-    /// Generates a uint. Values returned are over the full range of a uint, 
-    /// uint.MinValue to uint.MaxValue, (max exclusive).
-    /// 
-    /// This is the fastest method for generating a single random number because the underlying
-    /// random number generator algorithm generates 32 random bits that can be cast directly to 
-    /// a uint.
-    /// </summary>
-    /// <returns></returns>
-    public uint NextUInt()
-    {
-        uint t = (x ^ (x << 11));
-        x = y; y = z; z = w;
-        uint res = (w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)));
-        
-        if (res == uint.MaxValue) // ADDED BY FBESSETTE: We want to keep the MaxValue exclusive
-            res = 0;
-        return res;
-    }
-
-    /// <summary>
-    /// Generates a random int over the range 0 to int.MaxValue, exclusive.
-    /// 
-    /// The slight difference in range means this method is slightly faster than Next()
-    /// but is not functionally equivalent to System.Random.Next().
-    /// </summary>
-    /// <returns></returns>
-    public int NextInt()
-    {
-        uint t = (x ^ (x << 11));
-        x = y; y = z; z = w;
-        int res = (int)(0x7FFFFFFF & (w = (w ^ (w >> 19)) ^ (t ^ (t >> 8))));
-
-        if (res == int.MaxValue) // ADDED BY FBESSETTE: We want to keep the MaxValue exclusive
-            res = 0;
-        return res;
-    }
-
-    /// <summary>
-    /// Generates a single random bit.
-    /// This method's performance is improved by generating 32 bits in one operation and storing them
-    /// ready for future calls.
-    /// </summary>
-    /// <returns></returns>
-    public bool NextBool()
-    {
-        if (bitMask == 1)
-        {
-            // Generate 32 more bits.
-            uint t = (x ^ (x << 11));
-            x = y; y = z; z = w;
-            bitBuffer = w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
-
-            // Reset the bitMask that tells us which bit to read next.
-            bitMask = 0x80000000;
-            return (bitBuffer & bitMask) == 0;
-        }
-
-        return (bitBuffer & (bitMask >>= 1)) == 0;
-    }
-
-    #endregion
-
-    #region Public Methods [fbessette FixMath additions]
-
-
-    /// <summary> Returns a value between 0 and 1</summary>
-    public fix NextFixRatio() => new fix(NextInt()) / new fix(int.MaxValue);
+    /// <summary>Returns a uniformly random int value in the interval [-2147483647, 2147483647].</summary>
+    /// <returns>A uniformly random integer value.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public fix NextFix() => NextInt();
 
     /// <summary> Returns a value between 0 and max</summary>
-    public fix NextFix() => NextFixRatio() * fix.MaxValue;
-
-    /// <summary> Returns a value between 0 and max</summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public fix NextFix(fix max) => NextFixRatio() * max;
 
     /// <summary> Returns a value between min and max</summary>
-    public fix NextFix(fix min,  fix max) => (NextFixRatio() * (max - min)) + min;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public fix NextFix(fix min, fix max) => (NextFixRatio() * (max - min)) + min;
 
-    /// <summary> Returns a value between 0 and max (exclusive)</summary>
-    public int NextInt(int maxExclusive) => (int)NextFix(new fix(maxExclusive));
+    /// <summary>Returns a uniformly random int value in the interval [0, max).</summary>
+    /// <param name="max">The maximum value to generate, exclusive.</param>
+    /// <returns>A uniformly random int value in the range [0, max).</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int NextInt(int maxExclusive) => _random.NextInt(maxExclusive);
 
-    /// <summary> Returns a value between min and max (exclusive)</summary>
-    public int NextInt(int min, int maxExclusive) => (int)NextFix(new fix(min), new fix(maxExclusive));
+    /// <summary>Returns a uniformly random int value in the interval [min, max).</summary>
+    /// <param name="min">The minimum value to generate, inclusive.</param>
+    /// <param name="max">The maximum value to generate, exclusive.</param>
+    /// <returns>A uniformly random integer between [min, max).</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int NextInt(int min, int maxExclusive) => _random.NextInt(min, maxExclusive);
 
-    ///// <summary> Returns a value between 0 and max (exclusive)</summary>
-    //public uint NextUInt(uint max) => (uint)NextFix64(new Fix64(max));
 
-    ///// <summary> Returns a value between min and max (exclusive)</summary>
-    //public uint NextUInt(uint min, uint max) => (uint)NextFix64(new Fix64(min), new Fix64(max));
-
-    #endregion
+    public int2 NextInt2() => _random.NextInt2();
+    public int3 NextInt3() => _random.NextInt3();
+    public int4 NextInt4() => _random.NextInt4();
+    public uint2 NextUInt2() => _random.NextUInt2();
+    public uint3 NextUInt3() => _random.NextUInt3();
+    public uint4 NextUInt4() => _random.NextUInt4();
+    public bool2 NextBool2() => _random.NextBool2();
+    public bool3 NextBool3() => _random.NextBool3();
+    public bool4 NextBool4() => _random.NextBool4();
+    public int2 NextInt2(int2 max) => _random.NextInt2(max);
+    public int3 NextInt3(int3 max) => _random.NextInt3(max);
+    public int4 NextInt4(int4 max) => _random.NextInt4(max);
+    public uint2 NextUInt2(uint2 max) => _random.NextUInt2(max);
+    public uint3 NextUInt3(uint3 max) => _random.NextUInt3(max);
+    public uint4 NextUInt4(uint4 max) => _random.NextUInt4(max);
+    public int2 NextInt2(int2 min, int2 max) => _random.NextInt2(min, max);
+    public int3 NextInt3(int3 min, int3 max) => _random.NextInt3(min, max);
+    public int4 NextInt4(int4 min, int4 max) => _random.NextInt4(min, max);
+    public uint2 NextUInt2(uint2 min, uint2 max) => _random.NextUInt2(min, max);
+    public uint3 NextUInt3(uint3 min, uint3 max) => _random.NextUInt3(min, max);
+    public uint4 NextUInt4(uint4 min, uint4 max) => _random.NextUInt4(min, max);
 }
