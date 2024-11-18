4aeaa3237c76c813b2608d3a1aa20a6683ee9646
Fred <msfredb7@gmail.com>
2024-11-18 15:26:01 -0500

    [new] add NextElement extension methods to FixRandom

--- Runtime/Extensions/FixRandomExtensions.cs
+++ Runtime/Extensions/FixRandomExtensions.cs
@@ -7,15 +7,15 @@ using UnityEngine;
 
 public static class FixRandomExtensions
 {
-    public static fix2 NextFix2(this ref FixRandom random)                      => new fix2(random.NextFix(), random.NextFix());
-    public static fix2 NextFix2(this ref FixRandom random, fix2 max)            => new fix2(random.NextFix(max.x), random.NextFix(max.y));
-    public static fix2 NextFix2(this ref FixRandom random, fix2 min, fix2 max)  => new fix2(random.NextFix(min.x, max.x), random.NextFix(min.y, max.y));
-    public static fix3 NextFix3(this ref FixRandom random)                      => new fix3(random.NextFix(), random.NextFix(), random.NextFix());
-    public static fix3 NextFix3(this ref FixRandom random, fix3 max)            => new fix3(random.NextFix(max.x), random.NextFix(max.y), random.NextFix(max.z));
-    public static fix3 NextFix3(this ref FixRandom random, fix3 min, fix3 max)  => new fix3(random.NextFix(min.x, max.x), random.NextFix(min.y, max.y), random.NextFix(min.z, max.z));
-    public static fix4 NextFix4(this ref FixRandom random)                      => new fix4(random.NextFix(), random.NextFix(), random.NextFix(), random.NextFix());
-    public static fix4 NextFix4(this ref FixRandom random, fix4 max)            => new fix4(random.NextFix(max.x), random.NextFix(max.y), random.NextFix(max.z), random.NextFix(max.w));
-    public static fix4 NextFix4(this ref FixRandom random, fix4 min, fix4 max)  => new fix4(random.NextFix(min.x, max.x), random.NextFix(min.y, max.y), random.NextFix(min.z, max.z), random.NextFix(min.w, max.w));
+    public static fix2 NextFix2(this ref FixRandom random) => new fix2(random.NextFix(), random.NextFix());
+    public static fix2 NextFix2(this ref FixRandom random, fix2 max) => new fix2(random.NextFix(max.x), random.NextFix(max.y));
+    public static fix2 NextFix2(this ref FixRandom random, fix2 min, fix2 max) => new fix2(random.NextFix(min.x, max.x), random.NextFix(min.y, max.y));
+    public static fix3 NextFix3(this ref FixRandom random) => new fix3(random.NextFix(), random.NextFix(), random.NextFix());
+    public static fix3 NextFix3(this ref FixRandom random, fix3 max) => new fix3(random.NextFix(max.x), random.NextFix(max.y), random.NextFix(max.z));
+    public static fix3 NextFix3(this ref FixRandom random, fix3 min, fix3 max) => new fix3(random.NextFix(min.x, max.x), random.NextFix(min.y, max.y), random.NextFix(min.z, max.z));
+    public static fix4 NextFix4(this ref FixRandom random) => new fix4(random.NextFix(), random.NextFix(), random.NextFix(), random.NextFix());
+    public static fix4 NextFix4(this ref FixRandom random, fix4 max) => new fix4(random.NextFix(max.x), random.NextFix(max.y), random.NextFix(max.z), random.NextFix(max.w));
+    public static fix4 NextFix4(this ref FixRandom random, fix4 min, fix4 max) => new fix4(random.NextFix(min.x, max.x), random.NextFix(min.y, max.y), random.NextFix(min.z, max.z), random.NextFix(min.w, max.w));
 
     // methods that were not yet ported from Unity.Mathematic.Random
     //public static quaternion NextQuaternionRotation(this ref FixRandom random);
@@ -125,5 +125,17 @@ public static class FixRandomExtensions
     public static void Shuffle<T>(this ref FixRandom random, NativeList<T> collection) where T : unmanaged
     {
         Shuffle(ref random, collection.AsArray());
+    }
+
+    public static T NextElement<T>(this ref FixRandom random, NativeArray<T> collection) where T : unmanaged
+    {
+        int idx = random.NextInt(collection.Length);
+        return collection[idx];
+    }
+
+    public static T NextElement<T>(this ref FixRandom random, NativeList<T> collection) where T : unmanaged
+    {
+        int idx = random.NextInt(collection.Length);
+        return collection[idx];
     }
 }
